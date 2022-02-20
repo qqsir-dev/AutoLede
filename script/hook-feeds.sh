33bb3314be98aa0da196389474f86f7566472810
YG <54274847+qqsir-dev@users.noreply.github.com>
2022-02-20 10:09:07 +0800

    Update hook-feeds.sh

--- script/hook-feeds.sh
+++ script/hook-feeds.sh
@@ -23,16 +23,5 @@
 sed -i '/helloworld/d' feeds.conf.default
 echo "src-git helloworld https://github.com/fw876/helloworld.git" >> feeds.conf.default
 
-# Add luci-app-unblockneteasemusic
-rm -rf feeds/luci/applications/luci-app-unblockmusic
-git clone https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic.git feeds/luci/applications/luci-app-unblockmusic
-
-# Add luci-theme-argon
-rm -rf feeds/luci/themes/luci-theme-argon
-git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon feeds/luci/themes/luci-theme-argon
-git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config feeds/luci/applications/luci-app-argon-config
-rm -rf feeds/luci/themes/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
-cp -f $GITHUB_WORKSPACE/pics/bg1.jpg feeds/luci/themes/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
-
 # Update feeds
 ./scripts/feeds update -a
