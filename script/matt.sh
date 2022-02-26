4b4a225979184de0781d242924738c9e3344418c
YG <54274847+qqsir-dev@users.noreply.github.com>
2022-02-26 16:52:53 +0800

    Update matt.sh

--- script/matt.sh
+++ script/matt.sh
@@ -22,8 +22,8 @@ popd
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.199.1/g' package/base-files/files/bin/config_generate
-sed -i '/uci commit system/i\uci set system.@system[0].hostname='TimWrt'' package/lean/default-settings/files/zzz-default-settings
-sed -i "s/OpenWrt /YG @ TimWrt /g" package/lean/default-settings/files/zzz-default-settings
+sed -i '/uci commit system/i\uci set system.@system[0].hostname='MattWrt'' package/lean/default-settings/files/zzz-default-settings
+sed -i "s/OpenWrt /YG @ MattWrt /g" package/lean/default-settings/files/zzz-default-settings
 # find package/*/ feeds/*/ -maxdepth 6 -path "*luci-app-smartdns/luasrc/controller/smartdns.lua" | xargs -i sed -i 's/\"SmartDNS\")\, 4/\"SmartDNS\")\, 3/g' {} 
 # Test kernel 5.15
 # sed -i 's/5.10/5.15/g' target/linux/x86/Makefile
