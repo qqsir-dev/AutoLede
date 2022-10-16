051e33d0d809d1b96dcdfd710a7f0887f8e023ce
YG <54274847+qqsir-dev@users.noreply.github.com>
2022-10-16 13:57:22 +0800

    Update ledeMainRouterNginx.sh

--- script/ledeMainRouterNginx.sh
+++ script/ledeMainRouterNginx.sh
@@ -31,7 +31,7 @@ sed -i "/exit/iuci set network.wan.ifname=\'eth0\'\nuci set network.wan6.ifname=
 # sed -i $'s/option sfe_flow \'1\'/option sfe_flow \'0\'/g' feeds/luci/applications/luci-app-turboacc/root/etc/config/turboacc
 
 # Set Default Language - English & Nginx client_max_body_size 2048M
-sed -i "/exit/ised -i \"s/'zh_cn'/'en'/g\" /etc/config/luci\nsed -i \"s/'128M'/'2048M'/g\" /etc/nginx/uci.conf" package/base-files/files/etc/rc.local
+sed -i "/exit/ised -i \"s/'zh_cn'/'en'/g\" /etc/config/luci\nsed -i \"s/'128M'/'2048M'/g\" /etc/nginx/uci.conf\n" package/base-files/files/etc/rc.local
 
 # Set Nginx client_max_body_size 2048M
 # sed -i 's/128M/2048M/g' /etc/nginx/uci.conf
