36663331f471f58063251a2291baa935b9a6d325
YG <54274847+qqsir-dev@users.noreply.github.com>
2023-02-22 11:50:04 +0800

    Update ledeMainRouter.sh

--- script/ledeMainRouter.sh
+++ script/ledeMainRouter.sh
@@ -4,8 +4,13 @@
 mkdir package/community
 pushd package/community
 
-# Add OpenClash
-svn export https://github.com/vernesong/OpenClash/trunk/luci-app-openclash
+# Add luci-app-passwall
+git clone https://github.com/xiaorouji/openwrt-passwall
+cd openwrt-passwall
+git checkout 4fd4bf8
+cd ../
+git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2
+svn export https://github.com/xiaorouji/openwrt-passwall/branches/luci/luci-app-passwall
 
 # Add luci-theme-argon
 rm -rf ../../feeds/luci/themes/luci-theme-argon
