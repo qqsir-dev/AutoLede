225a395fc8c531a59a269581b9ade89089733250
YG <54274847+qqsir-dev@users.noreply.github.com>
2023-04-08 22:50:20 +0800

    Update ledeMainRouter.sh

--- script/ledeMainRouter.sh
+++ script/ledeMainRouter.sh
@@ -6,9 +6,6 @@ pushd package/community
 
 # Add luci-app-passwall
 git clone https://github.com/xiaorouji/openwrt-passwall
-cd openwrt-passwall
-git checkout 4fd4bf8
-cd ../
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2
 svn export https://github.com/xiaorouji/openwrt-passwall/branches/luci/luci-app-passwall
 
