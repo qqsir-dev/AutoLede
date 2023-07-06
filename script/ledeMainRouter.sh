6dd7f9e79046a20f0f9798c66d79ba4fb0a13f2b
YG <54274847+qqsir-dev@users.noreply.github.com>
2023-07-06 10:01:40 +0800

    Update ledeMainRouter.sh

--- script/ledeMainRouter.sh
+++ script/ledeMainRouter.sh
@@ -26,7 +26,7 @@ pushd -0
 dirs -v
 
 # 更改默认 Shell 为 zsh
-# sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
+sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
 
 # 修改本地时间格式
 sed -i 's/os.date()/os.date("%a %Y-%m-%d %H:%M:%S")/g' package/lean/autocore/files/*/index.htm
@@ -44,11 +44,6 @@ sed -i 's/256/1024/g' target/linux/x86/image/Makefile
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.50.2/g' package/base-files/files/bin/config_generate
-
-# 修改版本为编译日期
-date_version=$(date +"%y.%m.%d")
-orig_version=$(cat "package/lean/default-settings/files/zzz-default-settings" | grep DISTRIB_REVISION= | awk -F "'" '{print $2}')
-sed -i "s/${orig_version}/R${date_version} by Y.G/g" package/lean/default-settings/files/zzz-default-settings
 
 # Test kernel 6.1
 sed -i 's/5.15/6.1/g' target/linux/x86/Makefile
