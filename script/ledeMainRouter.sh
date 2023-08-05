6325b886575c2aedaf9b334cb9a0f6cabd034712
YG <54274847+qqsir-dev@users.noreply.github.com>
2023-08-05 18:11:09 +0800

    Update ledeMainRouter.sh

--- script/ledeMainRouter.sh
+++ script/ledeMainRouter.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #===============================================
 # Description: DIY script
-# File name: diy-script.sh
+# File name: ledeMainRouter.sh
 # Lisence: MIT
 # Author: P3TERX
 # Blog: https://p3terx.com
