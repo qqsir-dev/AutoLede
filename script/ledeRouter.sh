60c9af19a739b1896bc6a614bd2793a74f40d33a
yangjiey2k <42866354+yangjiey2k@users.noreply.github.com>
2023-09-08 22:16:37 +0800

    Update ledeRouter.sh

--- script/ledeRouter.sh
+++ script/ledeRouter.sh
@@ -128,7 +128,7 @@ sed -i "/exit/iuci set network.lan.netmask=\'255.255.255.0\'\nuci set network.la
 # 修改默认IP
 sed -i 's/192.168.1.1/192.168.1.202/g' package/base-files/files/bin/config_generate
 sed -i '/uci commit system/i\uci set system.@system[0].hostname='NibiruWrt'' package/lean/default-settings/files/zzz-default-settings
-sed -i "s/OpenWrt /YG @ NibiruWrt /g" package/lean/default-settings/files/zzz-default-settings
+sed -i "s/OpenWrt /KoNan @ NibiruWrt /g" package/lean/default-settings/files/zzz-default-settings
 
 # 更改默认 Shell 为 zsh
 # sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
