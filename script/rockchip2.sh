38ebb416c23f9e6529227dd90730e303909481dc
YG <54274847+qqsir-dev@users.noreply.github.com>
2024-01-18 13:50:54 +0800

    Update rockchip2.sh

--- script/rockchip2.sh
+++ script/rockchip2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #===============================================
-# Description: DIY script
-# File name: diy-script.sh
+# Description: R4S
+# File name: rockchip2.sh
 # Lisence: MIT
 # Author: qqsir
 #===============================================
