0496fbcd901973674ab8c5be7b8a9bfd9c7825df
YG <54274847+qqsir-dev@users.noreply.github.com>
2024-05-27 18:32:00 +0800

    Update rockchip.sh

--- script/rockchip.sh
+++ script/rockchip.sh
@@ -158,5 +158,5 @@ sed -i 's|admin\\|admin\\/services\\|g' feeds/luci/applications/luci-app-dockerm
 ./scripts/feeds install -a
 
 # GoLang
-rm -rf feeds/packages/lang/golang && git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
+# rm -rf feeds/packages/lang/golang && git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
 
