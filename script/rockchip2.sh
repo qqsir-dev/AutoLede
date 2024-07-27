7d4f43f5e0eb0c3b9d05ac5230e0e17c600fae8b
YG <54274847+qqsir-dev@users.noreply.github.com>
2024-07-27 09:03:46 +0800

    Update rockchip2.sh

--- script/rockchip2.sh
+++ script/rockchip2.sh
@@ -149,9 +149,6 @@ sed -i 's|admin\\|admin\\/services\\|g' feeds/luci/applications/luci-app-dockerm
 ./scripts/feeds update -a
 ./scripts/feeds install -a
 
-#Go1.22.x
-
-
 # Netdata ACL
 sed -i 's/allow connections from = localhost/allow connections from = * localhost/g' feeds/packages/admin/netdata/files/netdata.conf
 sed -i 's/allow dashboard from = localhost/allow dashboard from = * localhost/g' feeds/packages/admin/netdata/files/netdata.conf
