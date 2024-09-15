e1c188a239d817681db86397f0d5bec0d658563a
YG <54274847+qqsir-dev@users.noreply.github.com>
2024-09-15 14:39:30 +0800

    Update rockchip2.sh

--- script/rockchip2.sh
+++ script/rockchip2.sh
@@ -10,7 +10,7 @@
 sed -i 's/6.1/6.6/g' target/linux/rockchip/Makefile
 
 # 修改默认IP
-# sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
+# sed -i 's/192.168.11.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 
 # 更改默认 Shell 为 zsh
 # sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
