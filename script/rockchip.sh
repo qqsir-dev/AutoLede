15a2df769b8cfc6f8e909152ece332999ebca766
YG <54274847+qqsir-dev@users.noreply.github.com>
2024-10-05 12:48:51 +0800

    Update rockchip.sh

--- script/rockchip.sh
+++ script/rockchip.sh
@@ -14,7 +14,7 @@ sed -i "/exit/iuci set network.wan.ifname=\'eth2\'\nuci set network.wan.proto=\'
 
 
 # 修改默认IP
-sed -i 's/192.168.50.1/10.0.0.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.50.1/g' package/base-files/files/bin/config_generate
 
 # 更改默认 Shell 为 zsh
 # sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
