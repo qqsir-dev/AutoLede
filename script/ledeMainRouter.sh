c68a7016e8542b0172c0929763cf6b5db2efba6b
YG <54274847+qqsir-dev@users.noreply.github.com>
2024-10-06 21:49:56 +0800

    Update ledeMainRouter.sh

--- script/ledeMainRouter.sh
+++ script/ledeMainRouter.sh
@@ -124,7 +124,7 @@ find package/luci-theme-*/* -type f -name '*luci-theme-*' -print -exec sed -i '/
 # sed -i 's/services/vpn/g' feeds/luci/applications/luci-app-v2ray-server/luasrc/view/v2ray_server/*.htm
 
 # add Mihomo to feed
-echo "src-git mihomo https://github.com/morytyann/OpenWrt-mihomo.git;main" >> "feeds.conf.default"
+git clone https://github.com/morytyann/OpenWrt-mihomo package/luci-app-mihomo
 
 ./scripts/feeds update -a
 ./scripts/feeds install -a
