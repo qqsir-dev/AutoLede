81281726bc1a197ccdc028db2c4cb038ceb41eb7
YG <54274847+qqsir-dev@users.noreply.github.com>
2024-10-22 16:41:21 +0800

    Update init-settings.sh

--- script/init-settings.sh
+++ script/init-settings.sh
@@ -13,6 +13,9 @@ uci commit luci
 # Disable IPV6 ula prefix
 # sed -i 's/^[^#].*option ula/#&/' /etc/config/network
 
+#Add eth2-4 as Lan port
+sed -i "/list ports \'eth0\'/a\        list ports \'eth2\'\n        list ports \'eth3\'\n        list ports \'eth4\'" /etc/config/network
+
 # Check file system during boot
 # uci set fstab.@global[0].check_fs=1
 # uci commit fstab
