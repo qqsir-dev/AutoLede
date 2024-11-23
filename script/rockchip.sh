0181998e252933c6df06041d430b3e7e99ceba5e
YG <54274847+qqsir-dev@users.noreply.github.com>
2024-11-23 21:49:45 +0800

    Update rockchip.sh

--- script/rockchip.sh
+++ script/rockchip.sh
@@ -64,7 +64,7 @@ git_sparse_clone main https://github.com/Lienol/openwrt-package luci-app-filebro
 # git_sparse_clone master https://github.com/syb999/openwrt-19.07.1 package/network/services/msd_lite
 
 # 科学上网插件
-git_sparse_clone dev https://github.com/immortalwrt/homeproxy package/luci-app-homeproxy
+git_sparse_clone dev https://github.com/immortalwrt/homeproxy package/homeproxy
 # git clone --depth=1 https://github.com/fw876/helloworld package/luci-app-ssr-plus
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
