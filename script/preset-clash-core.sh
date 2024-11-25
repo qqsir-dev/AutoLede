d59e22cd64a4548201608e0a5b2cc53193dcda4b
YG <54274847+qqsir-dev@users.noreply.github.com>
2024-11-25 10:44:45 +0800

    Update preset-clash-core.sh

--- script/preset-clash-core.sh
+++ script/preset-clash-core.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mkdir -p files/etc/openclash/core
+PKG_PATCH="$GITHUB_WORKSPACE/openwrt/package/"
 
 CORE_VER="https://raw.githubusercontent.com/vernesong/OpenClash/core/dev/core_version"
 # CORE_TYPE=$(echo $WRT_TARGET | grep -Eiq "64|86" && echo "amd64" || echo "arm64")
@@ -14,11 +14,18 @@ GEO_MMDB="https://github.com/alecthw/mmdb_china_ip_list/raw/release/lite/Country
 GEO_SITE="https://github.com/Loyalsoldier/v2ray-rules-dat/raw/release/geosite.dat"
 GEO_IP="https://github.com/Loyalsoldier/v2ray-rules-dat/raw/release/geoip.dat"
 
-wget -qO- $CORE_DEV | tar xOvz > files/etc/openclash/core/clash
-wget -qO- $CORE_TUN | gunzip -c > files/etc/openclash/core/clash_tun
-wget -qO- $CORE_MATE | tar xOvz > files/etc/openclash/core/clash_meta
-wget -qO- $GEO_IP > files/etc/openclash/GeoIP.dat
-wget -qO- $GEO_SITE > files/etc/openclash/GeoSite.dat
-wget -qO- $GEO_MMDB > files/etc/openclash/Country.mmdb
+cd ./luci-app-openclash/root/etc/openclash/
 
-chmod +x files/etc/openclash/core/clash*
+	curl -sL -o Country.mmdb $GEO_MMDB && echo "Country.mmdb done!"
+	curl -sL -o GeoSite.dat $GEO_SITE && echo "GeoSite.dat done!"
+	curl -sL -o GeoIP.dat $GEO_IP && echo "GeoIP.dat done!"
+
+	mkdir ./core/ && cd ./core/
+
+	curl -sL -o meta.tar.gz $CORE_MATE && tar -zxf meta.tar.gz && mv -f clash clash_meta && echo "meta done!"
+	curl -sL -o tun.gz $CORE_TUN && gzip -d tun.gz && mv -f tun clash_tun && echo "tun done!"
+	curl -sL -o dev.tar.gz $CORE_DEV && tar -zxf dev.tar.gz && echo "dev done!"
+
+	chmod +x ./* && rm -rf ./*.gz
+
+	cd $PKG_PATCH && echo "openclash date has been updated!"
