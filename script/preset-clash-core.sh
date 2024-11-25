40f898e8bdda3d228f75ee17a5650d26f285d258
YG <54274847+qqsir-dev@users.noreply.github.com>
2024-11-25 19:29:33 +0800

    Update preset-clash-core.sh

--- script/preset-clash-core.sh
+++ script/preset-clash-core.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 mkdir -p files/etc/openclash/core
 CORE_VER="https://raw.githubusercontent.com/vernesong/OpenClash/core/dev/core_version"
 # CORE_TYPE=$(echo $WRT_TARGET | grep -Eiq "64|86" && echo "amd64" || echo "arm64")
@@ -16,8 +17,8 @@ curl -sL -o files/etc/openclash/Country.mmdb $GEO_MMDB && echo "Country.mmdb don
 curl -sL -o files/etc/openclash/GeoSite.dat $GEO_SITE && echo "GeoSite.dat done!"
 curl -sL -o files/etc/openclash/GeoIP.dat $GEO_IP && echo "GeoIP.dat done!"
 
-curl -sL -o files/etc/openclash/core/meta.tar.gz $CORE_MATE && tar -zxf meta.tar.gz && mv -f clash clash_meta && echo "meta done!"
-curl -sL -o files/etc/openclash/core/tun.gz $CORE_TUN && gzip -d tun.gz && mv -f tun clash_tun && echo "tun done!"
-curl -sL -o files/etc/openclash/core/dev.tar.gz $CORE_DEV && tar -zxf dev.tar.gz && echo "dev done!"
+curl -sL -o meta.tar.gz $CORE_MATE && tar -zxf files/etc/openclash/core/meta.tar.gz && mv -f clash clash_meta && echo "meta done!"
+curl -sL -o tun.gz $CORE_TUN && gzip -d files/etc/openclash/core/tun.gz && mv -f tun clash_tun && echo "tun done!"
+curl -sL -o dev.tar.gz $CORE_DEV && tar -zxf files/etc/openclash/core/dev.tar.gz && echo "dev done!"
 
 chmod +x files/etc/openclash/core/clash*
